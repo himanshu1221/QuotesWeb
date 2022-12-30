cf855107c1109d09ed63c0185314bf7819444864
Himanshu Chhatwal <himanshuchhatwal9295@gmail.com>
2022-12-30 15:30:30 +0530

    fetch api done

--- src/components/QuotesBox/Box.js
+++ src/components/QuotesBox/Box.js
@@ -1,19 +1,41 @@
-import React from 'react'
+import { async, useState } from 'react'
+import React, { useEffect } from 'react'
 import './box.css'
 
+
 const Box = () => {
+
+  const[Quotes,setQuotes] = useState([]);
+
+  const getQuotes = async() => {
+    const response = await fetch('https://type.fit/api/quotes')
+    setQuotes(await response.json());
+  }
+
+  useEffect(()=>{
+    getQuotes();
+  },[])
   return (
     <>
       <div className="box quotes-card">
-        <div className="box2 Quotes-Box">
-          <span className="text">
-          Everyday is a new day
-          </span>
-          <span className="author-name">~Himanshu chhatwal</span>
+      {
+        Quotes.map((e)=>{
+          return(
+            <>
+          <div className="box2 Quotes-Box">
+          <div className="text">
+          {e.text}
+          </div>
+          <div className="author-name">~{e.author}</div>
           <button className='button'>New Quote</button>
         </div>
+            </>
+          )
+        })
+      }
       </div>
     </>
+
   )
 }
 
